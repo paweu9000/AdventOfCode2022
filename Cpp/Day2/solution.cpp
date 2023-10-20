ce27be268071d6a138ca5e8e972b90d1fa37dc74
paweu9000 <wolfenorrin5@gmail.com>
2023-10-20 12:31:33 +0200

    cpp day2 solution1

---
+++ Cpp/Day2/solution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+enum game {
+    rock = 1,
+    paper = 2,
+    scissors = 3
+};
+
+int make_choice(char c) {
+    if (c == 'A' || c == 'X') return game(rock);
+    else if (c == 'B' || c == 'Y') return game(paper);
+    else return game(scissors);
+}
+
+int get_winner(char enemy, char player) {
+    int p = make_choice(player), e = make_choice(enemy);
+    if (p == e) return p+3;
+    else if (e == 3 && p == 1) return 7;
+    else if (p == 3 && e == 1) return 3;
+    else if (p < e) return p;
+    else return p+6;
+}
+
+int main() {
+    ifstream file;
+    file.open("rps.txt");
+    string line;
+    int sum = 0;
+    while (getline(file, line)) {
+        sum += get_winner(line[0], line[2]);
+    }
+    cout << sum;
+    return 0;
+}
